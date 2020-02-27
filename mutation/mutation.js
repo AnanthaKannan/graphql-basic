d77c5e557312b5029834f8a2a31b20c302b3525f
AnanthaKannan <sreeananthakannan@gmail.com>
2020-02-28 00:48:04 +0530

    graphqlNonNull_added

--- mutation/mutation.js
+++ mutation/mutation.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLID, GraphQLInt,
+const { GraphQLObjectType, GraphQLID, GraphQLInt, GraphQLNonNull,
      GraphQLSchema, GraphQLList, GraphQLString } = graphql
 
 
@@ -14,8 +14,8 @@ module.exports = new GraphQLObjectType({
         addAuhtor:{
             type: type.AuthorType,
             args:{
-                name: { type:GraphQLString },
-                age:{ type:GraphQLInt }
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age:{ type: new GraphQLNonNull(GraphQLInt) }
             },
             resolve(parent, args){
                 let author = new Author({
@@ -28,9 +28,9 @@ module.exports = new GraphQLObjectType({
         addBook:{
             type:type.BookType,
             args:{
-                name:{ type: GraphQLString},
-                genre:{ type: GraphQLString},
-                authorid:{type: GraphQLID}
+                name:{ type: new GraphQLNonNull(GraphQLString)},
+                genre:{ type: new GraphQLNonNull(GraphQLString)},
+                authorid:{type: new GraphQLNonNull(GraphQLID)}
             },
             resolve(parent, args){
                 let book = new Book(args);
